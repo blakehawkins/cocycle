f5ed8e7c21148c0f66a8e2cda848f79aa6d122f2
David Jones <david.jones@fmail.co.uk>
2014-06-10 23:05:47 +0100

    Add group links to the navigation bar
    - DRY up the template with a nav_link helper method, which adds a
      conditional "active" class to the <li> tag for the current page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # Create a link within a list item with a conditional 'active' class.
+  def nav_link(body, path)
+    content_tag(:li, class: ('active' if current_page? path)) do
+      link_to body, path
+    end
+  end
 end
