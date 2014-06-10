412e4dca8a1daf7dca964fafe9498ba1a9b727d0
David Jones <david.jones@fmail.co.uk>
2014-06-10 14:40:10 +0100

    Validate group start time

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ActiveRecord::Base
   belongs_to :route
+
+  validates :start_time, inclusion: 0...24.hours
 end
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should require a start time to save group' do
+    group = Group.new
+    assert_not group.save
+  end
+
+  test 'should require a start time in seconds since midnight' do
+    t = Time.utc 2014, 06, 10, 07, 45, 30
+
+    group = Group.new start_time: t
+    assert_not group.save
+
+    group.start_time = t.sec.seconds + t.min.minutes + t.hour.hours
+    assert group.save
+  end
+
+  test 'should not create a group with an unreasonable start_time' do
+    group = Group.new start_time: 24.hours
+    assert_not group.save
+  end
+
+  test 'should round start_time to second precision' do
+    group = Group.create start_time: 12.hours + Math::PI.seconds
+    assert_equal group.start_time, 12.hours + 3.seconds
+  end
 end
