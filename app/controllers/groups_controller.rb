aee1335002bde4d3c9f87794c4eab24a46f4ef14
David Jones <david.jones@fmail.co.uk>
2014-06-10 20:04:05 +0100

    Add RESTful group actions
    - Add failing test to test/controller/group_controller_test.rb
    - Add action to app/controllers/group_controller.rb
    - Add template to app/views/groups/ if required
    - Repeat until all actions are tested

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,57 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: %i(show edit update destroy)
+
+  # GET /groups
+  def index
+    @groups = Group.all
+  end
+
+  # POST /groups
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      redirect_to @group, notice: 'Group was created successfully.'
+    else
+      render :new
+    end
+  end
+
+  # GET /groups/new
+  def new
+    @group = Group.new
+  end
+
+  # GET /groups/1/edit
+  def edit
+  end
+
+  # GET /groups/1
+  def show
+  end
+
+  # PATCH /groups/1
+  def update
+    if @group.update(group_params)
+      redirect_to @group, notice: 'Group was updated successfully.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /groups/1
+  def destroy
+    @group.destroy
+    redirect_to groups_url, notice: 'Group was deleted.'
+  end
+
+  private
+
+  def set_group
+    @group = Group.find params[:id]
+  end
+
+  def group_params
+    params.require(:group).permit(:start_time)
+  end
 end
--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
 class GroupsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @group = groups :morning
+  end
+
+  test 'should get index' do
+    get :index
+    assert_response :success
+    assert_not_nil assigns :groups
+  end
+
+  test 'should create group' do
+    assert_difference -> { Group.count } do
+      post :create, group: { start_time: 12.hours }
+    end
+
+    assert_redirected_to group_path assigns :group
+  end
+
+  test 'should not create group with missing params' do
+    assert_no_difference -> { Group.count } do
+      post :create, group: { start_time: nil }
+    end
+  end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+    assert_not_nil assigns :group
+  end
+
+  test 'should get edit' do
+    get :edit, id: @group
+    assert_response :success
+  end
+
+  test 'should show group' do
+    get :show, id: @group
+    assert_response :success
+  end
+
+  test 'should update group' do
+    patch :update, id: @group, group: { start_time: 13.hours + 13.minutes }
+    assert_equal Group.find(@group).start_time, 13.hours + 13.minutes
+
+    assert_redirected_to group_path assigns :group
+  end
+
+  test 'should destroy group' do
+    assert_difference -> { Group.count }, -1 do
+      delete :destroy, id: @group
+    end
+
+    assert_redirected_to groups_path
+  end
 end
