3024d330f46dae6443883165454fceccec67df8e
David Jones <david.jones@fmail.co.uk>
2014-06-10 17:06:42 +0100

    Validate route coordinates

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,4 @@
 class Route < ActiveRecord::Base
+  validates :start_lat, :finish_lat, inclusion: -90..90
+  validates :start_long, :finish_long, inclusion: -180..180
 end
--- test/models/route_test.rb
+++ test/models/route_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class RouteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @route = routes :one
+  end
+  test 'should be valid with start and end coordinates' do
+    assert @route.valid?
+  end
+
+  test 'should require start and finish coords to save route' do
+    assert_not @route.dup.tap { |r| r.start_lat = nil }.save
+    assert_not @route.dup.tap { |r| r.finish_long = nil }.save
+  end
+
+  test 'should not save a route with unreasonable coordinates' do
+    assert @route.dup.tap { |r| r.start_long = 180 }.save,
+           'Coordinate with -180 <= longitude <= 180 was not allowed'
+    assert_not @route.dup.tap { |r| r.start_long = 181 }.save,
+               'Coordinate with longitude > 180 was allowed'
+
+    assert @route.dup.tap { |r| r.finish_long = -90 }.save,
+           'Coordinate with -90 <= latitude <= 90 was not allowed'
+    assert_not @route.dup.tap { |r| r.finish_lat = -91 }.save,
+               'Coordinate with latitude < -90 was allowed'
+  end
 end
