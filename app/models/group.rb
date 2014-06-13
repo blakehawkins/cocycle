32f92e6bf38a4a37b1f88ce380692aacda92cc54
David Jones <david.jones@fmail.co.uk>
2014-06-13 22:00:28 +0100

    Add string getter and setter for group time

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,21 @@
 class Group < ActiveRecord::Base
+  TIME_REGEXP = /\A
+    (?<hour>[01]\d|2[0-3])      # hour 00..23
+    :?                          # optional separator
+    (?<minute>[0-5]\d)          # minute 00..59
+  \Z/x
+
   belongs_to :route
 
   validates :start_time, inclusion: 0...24.hours
+
+  def time=(str)
+    self.start_time = nil unless TIME_REGEXP.match str do |match|
+      self.start_time = match['hour'].to_i.hours + match['minute'].to_i.minutes
+    end
+  end
+
+  def time
+    Time.at(start_time).utc.to_s :time
+  end
 end
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
+  def setup
+    @group = groups :morning
+  end
+
   test 'should require a start time to save group' do
-    group = Group.new
-    assert_not group.save
+    @group.start_time = nil
+    assert_not @group.save
   end
 
   test 'should require a start time in seconds since midnight' do
@@ -24,5 +28,34 @@ class GroupTest < ActiveSupport::TestCase
   test 'should round start_time to second precision' do
     group = Group.create start_time: 12.hours + Math::PI.seconds
     assert_equal group.start_time, 12.hours + 3.seconds
+  end
+
+  test 'should not convert an invalid time to seconds since midnight' do
+    %w(2400 123 00:60 12:0 1am 12:30pm 7:40\ am).each do |time|
+      @group.time = time
+      assert_nil @group.start_time
+      assert_not @group.save
+    end
+  end
+
+  test 'should convert a valid time to seconds since midnight' do
+    { 0.minutes             => '00:00',
+      4.hours + 56.minutes  => '0456',
+      12.hours + 34.minutes => '1234',
+      13.hours + 37.minutes => '13:37',
+      1.day - 1.minute      => '23:59'
+    }.each do |expected, time|
+      @group.time = time
+      assert_equal expected, @group.start_time
+      assert @group.save
+    end
+  end
+
+  test 'should convert seconds since midnight to a time string' do
+    @group.time = '1234'
+    assert_equal '12:34', @group.time
+
+    @group.start_time = 11.hours + 11.minutes + 11.seconds
+    assert_equal '11:11', @group.time
   end
 end
