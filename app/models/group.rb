d91a3269334fd70dee7fb2321dddd7994d2730c4
David Jones <david.jones@fmail.co.uk>
2014-06-21 00:43:20 +0100

    Add basic time parsing
    -    1 -> 01:00
    -   12 -> 12:00
    - 1234 -> 12:34

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,14 @@ class Group < ActiveRecord::Base
   belongs_to :location
 
   validates :time, format: /\A([01]\d|2[0-3]):([0-5]\d)\Z/
+
+  before_validation do
+    if time.respond_to?(:match) && time.match(/\A\d+\z/)
+      case time.length
+      when 1 then time.prepend('0') << ':00'
+      when 2 then time << ':00'
+      when 4 then time.insert 2, ':'
+      end
+    end
+  end
 end
