b49991884eb9c89221ba396f92d6c20eec1dbb0c
David Jones <david.jones@fmail.co.uk>
2014-06-24 12:55:18 +0100

    Add more flexible 12-hour time parsing
    - 1am -> 01:00
    - 12 PM -> 12:00
    - 1:23p -> 13:23

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,13 +3,29 @@ class Group < ActiveRecord::Base
 
   validates :time, format: /\A([01]\d|2[0-3]):([0-5]\d)\Z/
 
-  before_validation do
-    if time.respond_to?(:match) && time.match(/\A\d+\z/)
-      case time.length
-      when 1 then time.prepend('0') << ':00'
-      when 2 then time << ':00'
-      when 4 then time.insert 2, ':'
-      end
-    end
+  before_validation :convert_12_hour_time, :convert_digit_time
+
+  private
+
+  def convert_12_hour_time
+    time.match(/\A
+      (?<h>1[0-2]?|0?[1-9]) # hour: 1, 10-12, 2-9, 01-09
+      (?<m>:[0-5]\d)?       # min: optional :00-:59
+      \ ?                   # optional space
+      (?:[aA]|(?<p>[pP]))   # afternoon?
+      [mM]?                 # optional meridian suffix
+    \z/x) do |m|
+      h = m[:h].to_i
+      time.replace sprintf '%02d', (h < 12 ? h : 0) + (m[:p] ? 12 : 0)
+      time << (m[:m] || ':00')
+    end if time.respond_to?(:match)
+  end
+
+  def convert_digit_time
+    case time.length
+    when 1 then time.prepend('0') << ':00'
+    when 2 then time << ':00'
+    when 4 then time.insert 2, ':'
+    end if time.respond_to?(:match) && time.match(/\A\d+\z/)
   end
 end
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -11,7 +11,7 @@ class GroupTest < ActiveSupport::TestCase
   end
 
   test 'should not save group with invalid time' do
-    %w(12:345 :34 12: 24:00 00:60 ab:cd).each do |t|
+    %w(12:345 :34 12: 24:00 00:60 ab:cd 13:00pm 00:00\ am).each do |t|
       @group.time = t
       assert_not @group.save, "Group saved with time: #{t.inspect}"
     end
@@ -35,5 +35,25 @@ class GroupTest < ActiveSupport::TestCase
   test 'should convert a 4-digit military time' do
     assert @group.update time: '0123'
     assert_equal '01:23', @group.time
+  end
+
+  test 'should convert a 12-hour morning time' do
+    assert @group.update time: '1:23 am'
+    assert_equal '01:23', @group.time
+  end
+
+  test 'should convert a 12-hour afternoon time' do
+    assert @group.update time: '1:23pm'
+    assert_equal '13:23', @group.time
+  end
+
+  test 'should convert a 1-digit 12-hour hour' do
+    assert @group.update time: '9 pm'
+    assert_equal '21:00', @group.time
+  end
+
+  test 'should convert a 2-digit 12-hour hour' do
+    assert @group.update time: '12 am'
+    assert_equal '00:00', @group.time
   end
 end
